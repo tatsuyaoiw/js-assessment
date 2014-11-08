f985c24dab76d0f118cabf16a8b466654bcb9e5b
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-11-09 08:59:46 +0900

    objects and context

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      var propNames = Object.getOwnPropertyNames(obj);
+      var ret = [];
+      for (var i = 0; i < propNames.length; i++) {
+        var propName = propNames[i];
+        var propValue = obj[propName];
+        ret.push(propName + ': ' + propValue);
+      }
+      return ret;
     }
   };
 });
