0fd7c9d8abb5954f73d8c985cf0b6292075459fa
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-11-12 08:37:37 +0900

    recursion

--- app/recursion.js
+++ app/recursion.js
@@ -3,11 +3,51 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var listOfFiles = [];
+      var dirs = [];
 
+      findFiles(data);
+
+      function findFiles(dir) {
+        dirs.push(dir.dir);
+
+        for (var i = 0; i < dir.files.length; i++) {
+          var file = dir.files[i];
+          if (typeof file === 'string') {
+            if (!dirName || dirs.indexOf(dirName) > -1) {
+              listOfFiles.push(file);
+            }
+          } else {
+            findFiles(file);
+          }
+        }
+
+        dirs.pop(dir.dir);
+      }
+
+      return listOfFiles;
     },
 
     permute: function(arr) {
+      var ret = [];
+      var tmp = [];
 
+      function process(arr) {
+        for (var i = 0; i < arr.length; i++) {
+          var elem = arr.splice(i, 1)[0];
+          tmp.push(elem);
+          if (arr.length !== 0) {
+            process(arr);
+          } else {
+            ret.push(tmp.slice());
+          }
+          arr.splice(i, 0, elem);
+          tmp.pop();
+        }
+      }
+
+      process(arr);
+      return ret;
     }
   };
 });
