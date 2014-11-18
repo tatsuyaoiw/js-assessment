fd0a8ceec774c0c605cb81e33ccd3a817fd26555
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-11-19 07:48:15 +0900

    counter

--- app/count.js
+++ app/count.js
@@ -3,6 +3,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timeoutId;
+
+      function process() {
+        console.log(start);
+        start++;
+
+        if (start <= end) {
+          timeoutId = setTimeout(process, 100);
+        }
+      }
+
+      process();
+
+      var counter = {
+        cancel: function() {
+          timeoutId && clearTimeout(timeoutId);
+        }
+      };
+
+      return counter;
     }
   };
 });
